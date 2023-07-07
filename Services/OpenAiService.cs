a3289eded1f821443eacf9a30ae9700818390c9c
張　一寧 <zhang_yining@member.toponetec.com>
2023-07-07 13:44:10 +0900

    制御文の改修2

--- Services/OpenAiService.cs
+++ Services/OpenAiService.cs
@@ -22,7 +22,7 @@ public class OpenAiService
     /// System prompt to send with user prompts to instruct the model for summarization
     /// </summary>
     private readonly string _summarizePrompt = @"
-        この文を長さが６文字以内のタイトルに作ってください。'「」。'などの符号を使用しないでください。" + Environment.NewLine;
+        以下の文について、長さが文字10個以内のタイトルに作ってください。鉤括弧、句点などの符号を使用しないこと。作れない場合は文の内のキーワード1個を挙げましょう" + Environment.NewLine;
 
     /// <summary>
     /// Creates a new instance of the service.
